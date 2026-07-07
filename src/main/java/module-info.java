3b57cb0dd904ca09bad094f9b710b53dad7ef212
FelipeMeirelesDev <felipegoncalvesmeireles2003@gmail.com>
2026-07-07 12:59:20 -0300

    configurando o projeto e suas dependencias do maven

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,19 @@
 module com.meirelesdev.gestao_concreta {
-    requires javafx.controls;
-    requires javafx.fxml;
 
+        requires javafx.controls;
+        requires javafx.fxml;
+        requires java.desktop;
+        requires java.sql;
+        //requires com.github.librepdf.openpdf;
 
-    opens com.meirelesdev.gestao_concreta to javafx.fxml;
-    exports com.meirelesdev.gestao_concreta;
+        exports com.meirelesdev.gestao_concreta;
+        exports com.meirelesdev.gestao_concreta.controller;
+        exports com.meirelesdev.gestao_concreta.model;
+
+        opens com.meirelesdev.gestao_concreta to javafx.fxml;
+        opens com.meirelesdev.gestao_concreta.controller to javafx.fxml;
+
+        opens com.meirelesdev.gestao_concreta.model to javafx.base;
+        exports com.meirelesdev.gestao_concreta.dao;
+        opens com.meirelesdev.gestao_concreta.dao to javafx.fxml;
 }
